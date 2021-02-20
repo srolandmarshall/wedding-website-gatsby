701db3529a92a0f85bc5869de73c907e23d69c7f
srolandmarshall <samuel.r.marshall@gmail.com>
2021-02-19 20:46:51 -0500

    removed CTA

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,13 +32,13 @@ const Header = () => {
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
+          {/* <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {/* {cta} */}
+                {cta}
               </Link>
             </span>
-          </p>
+          </p> */}
         </Fade>
       </Container>
     </section>
